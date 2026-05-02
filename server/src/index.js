9f5999df094ab957be2460831d3eec32df5d4978
Reyan Andrea <reyanandre@gmail.com>
2026-05-02 17:58:38 +0700

    feat: setup express server with routes & uploads

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,41 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import {
+  fileURLToPath
+} from "url";
+import authRoutes from "./routes/authRoutes.js";
+import laporanRoutes from "./routes/laporanRoutes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __filename = fileURLToPath(
+  import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Middleware
-app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(cors());
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/laporan", laporanRoutes);
 
 // Test route
 app.get("/", (req, res) => {
-  res.json({ message: "SmartWaste API is running yaaa!" });
+  res.json({
+    message: "SmartWaste API is running!"
+  });
 });
 
 app.listen(PORT, () => {
