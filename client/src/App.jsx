090e4235eaa0a7c4980f3bfbf4600dff462dc243
Reyan Andrea <reyanandre@gmail.com>
2026-05-04 13:09:49 +0700

    feat: tambah route admin ke App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,39 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Warga pages
-import Welcomepage from "./pages/Welcomepage";
-import Loginpage from "./pages/Loginpage";
-import Registerpage from "./pages/Registerpage";
-import DashboardPage from "./pages/DashboardPage";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import WelcomePage from './pages/WelcomePage';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
+import ReportPage from './pages/ReportPage';
+import HistoryPage from './pages/HistoryPage';
 import DetailReportPage from "./pages/DetailReportPage";
-import Historypage from "./pages/Historypage";
+import NotificationPage from "./pages/NotificationPage";
 import ProfilePage from "./pages/ProfilePage";
 import EditProfilePage from "./pages/EditProfilePage";
 import ChangePasswordPage from "./pages/ChangePasswordPage";
-import NotificationPage from "./pages/NotificationPage";
-import Reportpage from "./pages/Reportpage";
-import Landingpage from "./pages/Landingpage";
-
-// Admin pages
-import AdminApp from "./admin_pages/AdminApp";
+import AdminApp from './admin_pages/AdminApp';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Public */}
-        <Route path="/" element={<Welcomepage />} />
-        <Route path="/login" element={<Loginpage />} />
-        <Route path="/register" element={<Registerpage />} />
-        <Route path="/landing" element={<Landingpage />} />
-
-        {/* Warga */}
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/report" element={<Reportpage />} />
-        <Route path="/detail-report/:id" element={<DetailReportPage />} />
-        <Route path="/history" element={<Historypage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/edit-profile" element={<EditProfilePage />} />
-        <Route path="/change-password" element={<ChangePasswordPage />} />
-        <Route path="/notification" element={<NotificationPage />} />
-
-        {/* Admin */}
-        <Route path="/admin/*" element={<AdminApp />} />
-      </Routes>
+      <div style={{ maxWidth: '430px', margin: '0 auto', minHeight: '100vh', background: 'white' }}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/welcome" replace />} />
+          <Route path="/welcome" element={<WelcomePage />} />
+          <Route path="/landing" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/report" element={<ReportPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/detail/:id" element={<DetailReportPage />} />
+          <Route path="/notif" element={<NotificationPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/edit-profile" element={<EditProfilePage />} />
+          <Route path="/change-password" element={<ChangePasswordPage />} />
+          <Route path="/admin/*" element={<AdminApp />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
