ad6e33e37a2841ba5d427b6e6eeee7225e5c76a3
Reyan Andrea <reyanandre@gmail.com>
2026-05-04 21:52:10 +0700

    fix baseurl

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://server-production-9743.up.railway.app",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
