3136dccabc525b0e874673f5345fc87039d43653
Reyan Andrea <reyanandre@gmail.com>
2026-05-05 05:07:11 +0700

    fix: hapus double /api di baseURL axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smartwastee.vercel.app/api",
+  baseURL: "https://smartwastee.vercel.app",
 });
 
 api.interceptors.request.use((config) => {
